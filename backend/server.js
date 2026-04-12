9da31e137bfaa18885f7013ac6664feeb2f4ae6e
Gopika <gopikasankar05@gmail.com>
2026-04-12 17:16:30 +0530

    fix cors trailing slash

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://social-media-app-vuqs.vercel.app'
+  ]
+}));
 
-app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
@@ -19,6 +24,6 @@ app.use('/api/comments', require('./routes/comments'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,22 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api'
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return req;
+// });
+
+// export default API;
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://social-media-app-hi7s.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
